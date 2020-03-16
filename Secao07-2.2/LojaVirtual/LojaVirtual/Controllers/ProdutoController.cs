7a817b5dff50385bd8a18e51cc20bdb3cfac7496
Kewin Kuster <kevinkiister0@gmail.com>
2020-03-16 10:35:19 -0300

    Add secao 8 exercicio 1

---
+++ Secao07-2.2/LojaVirtual/LojaVirtual/Controllers/ProdutoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LojaVirtual.Models;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public ActionResult Visualizar()
+        {
+            Produto produto = GetProduto();
+
+            return View(produto);
+           // return new ContentResult() { Content = "<h3>Produto -> Visualizar</h3>", ContentType = "text/html" };
+        }
+        private Produto GetProduto()
+        {
+            return new Produto()
+            {
+                Id = 1,
+                Nome = "Xbox 360",
+                Descricao = "Jogue em 4K",
+                Valor = 2000.0M
+            };
+        }
+
+    }
+}
---
+++ Secao07/LojaVirtual/LojaVirtual/Controllers/ProdutoController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoController
+    {
+        public string Visualizar()
+        {
+            return "Produto -> Visualizar";
+        }
+    }
+}
